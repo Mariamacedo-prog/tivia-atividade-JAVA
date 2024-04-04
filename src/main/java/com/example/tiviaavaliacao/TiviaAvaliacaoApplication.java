5e2c383d4ea286a5105fcd7add1444a6767c97d2
mariamacedo2xx@gmail.com <mariamacedo2xx@gmail.com>
2024-04-03 21:58:44 -0300

    first commit

---
+++ src/main/java/com/example/tiviaavaliacao/TiviaAvaliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiviaavaliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiviaAvaliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiviaAvaliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tiviaavaliacao/TiviaAvaliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiviaavaliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiviaAvaliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
